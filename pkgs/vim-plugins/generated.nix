c1449d350506a6ab51ec616c60305d39eb44b87f
Foster McLane <fkmclane@gmail.com>
2020-05-15 10:44:02 -0400

    fooster.vimPlugins.hexmode: init at 2018-11-01

--- pkgs/vim-plugins/generated.nix
+++ pkgs/vim-plugins/generated.nix
@@ -4,5 +4,16 @@
 let
   packages = ( self:
 {
+  hexmode = buildVimPluginFrom2Nix {
+    pname = "hexmode";
+    version = "2018-11-01";
+    src = fetchFromGitHub {
+      owner = "fidian";
+      repo = "hexmode";
+      rev = "06b7b17578df26d853afe17f38e0e711a8fb094c";
+      sha256 = "0gq6pj0c25p4p2k24ic4za3qdw2ai0j1wsgynlz94yc53s97jmhf";
+    };
+  };
+
 });
 in lib.fix' (lib.extends overrides packages)
