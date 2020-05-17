05e2ac27feab2b9d7172b13c864b4a051aff19d5
Foster McLane <fkmclane@gmail.com>
2020-05-17 08:22:17 -0400

    add lily git config

--- config/lily.nix
+++ config/lily.nix
@@ -16,9 +16,27 @@
   };
 
   home-manager.users.lily = { pkgs, ... }: {
-    programs.fish.enable = true;
-    programs.fish.functions = {
-      fish_greeting = "";
+    programs.fish = {
+      enable = true;
+      functions = {
+        fish_greeting = "";
+      };
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "Foster McLane";
+      userEmail = "fkmclane@gmail.com";
+      aliases = {
+        kill = "!sh -c 'git reset HEAD --hard && git clean -xdf'";
+        subupd = "submodule update --init";
+        subpull = "submodule foreach git pull";
+        uppull = "pull upstream master";
+      };
+      delta = {
+        enable = true;
+        options = [ "--dark" ];
+      };
     };
   };
 }
