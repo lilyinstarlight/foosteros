059f2857c88441ddb9f86af426f52b1900dd3196
Foster McLane <fkmclane@gmail.com>
2020-06-04 10:56:21 -0400

    pkgs/swaynag-battery: fix with store path for sway{nag,msg}

--- pkgs/swaynag-battery/default.nix
+++ pkgs/swaynag-battery/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildGoModule, fetchFromGitHub }:
+{ stdenv, pkgs, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
   pname = "swaynag-battery";
@@ -14,6 +14,11 @@ buildGoModule rec {
     sha256 = "0bmw8yy8b5hw9c6kydznd8hig8l10d3gk6c5myah3zsf0d91ksxa";
   };
   vendorSha256 = "0kwzx3xf6j5z0zzmybxfpmnz8ll7jyh9fkrfjri7mlch77gn7ml7";
+
+  patchPhase = ''
+    sed -i -e 's#\(exec.Command("\)swaynag\("\)#\1${pkgs.sway}/bin/swaynag\2#g' swaynag.go
+    sed -i -e 's#\(exec.Command("\)swaymsg\("\)#\1${pkgs.sway}/bin/swaymsg\2#g' swaymsg.go
+  '';
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/m00qek/swaynag-battery";
