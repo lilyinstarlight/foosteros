04d5228e4cb09434e56d5013ef1db339d21b2793
Lily Foster <lily@lily.flowers>
2021-05-28 11:19:44 -0400

    pkgs/materia-theme: use resvg instead of inkscape

--- pkgs/materia-theme/default.nix
+++ pkgs/materia-theme/default.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 
 pkgs.materia-theme.overrideAttrs (attrs: rec {
-  nativeBuildInputs = attrs.nativeBuildInputs ++ (with pkgs; [ bc inkscape optipng ]);
+  nativeBuildInputs = attrs.nativeBuildInputs ++ (with pkgs; [ bc resvg optipng ]);
 
   dontConfigure = true;
 
@@ -10,6 +10,10 @@ pkgs.materia-theme.overrideAttrs (attrs: rec {
     sed -i install.sh \
       -e "s|if .*which gnome-shell.*;|if true;|" \
       -e "s|CURRENT_GS_VERSION=.*$|CURRENT_GS_VERSION=${pkgs.lib.versions.majorMinor pkgs.gnome3.gnome-shell.version}|"
+    for script in render-assets.sh src/*/render-asset.sh; do
+      sed -i "$script" \
+        -e "s|\brendersvg\b|resvg|g"
+    done
     ./change_color.sh -t $out/share/themes -o Materia-Fooster <(echo -e "MATERIA_COLOR_VARIANT=dark\nSEL_BG=F29BD4\nFG=EEEEEE\nBG=181818\nHDR_BG=2d2d2d\nHDR_FG=EEEEEE\nMATERIA_SURFACE=343434\nMATERIA_VIEW=242424\n")
   '';
 })
