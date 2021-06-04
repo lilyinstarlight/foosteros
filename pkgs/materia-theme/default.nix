d16eaf720536cd3765e118e2653f75f5eb6bf67c
Lily Foster <lily@lily.flowers>
2021-06-04 07:40:45 -0400

    pkgs/materia-theme: fix qgtkstyle issues

--- pkgs/materia-theme/default.nix
+++ pkgs/materia-theme/default.nix
@@ -3,6 +3,10 @@
 materia-theme.overrideAttrs (attrs: rec {
   nativeBuildInputs = attrs.nativeBuildInputs ++ [ bc resvg optipng ];
 
+  propagatedUserEnvPkgs = attrs.propagatedUserEnvPkgs ++ [
+    gnome.gnome-themes-extra  # apparently needed for gtk2 style (at least as used by QGtkStyle)
+  ];
+
   dontConfigure = true;
 
   installPhase = ''
