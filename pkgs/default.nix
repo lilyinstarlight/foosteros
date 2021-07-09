b9d5e03201a2f44f0a2f22c7fc5e177764f68a82
Lily Foster <lily@lily.flowers>
2021-07-09 15:47:07 -0400

    pkgs: add libsforqt5 overlay and adjust package dependency references for non-overlay usage

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,10 +11,14 @@ let
   in self;
   python3Packages = recurseIntoAttrs python3.pkgs;
 
+  libsForQt5 = pkgs.libsForQt5 // {
+    drumstick = pkgs.libsForQt5.callPackage ./drumstick { inherit (pkgs.libsForQt5) drumstick; };
+  };
+
   vimPlugins = pkgs.vimPlugins.extend (self: super: callPackage ./vim-plugins {});
 in
 
-{
+rec {
   fooster-backgrounds = callPackage ./backgrounds {};
   fooster-materia-theme = callPackage ./materia-theme {};
   fpaste = python3Packages.callPackage ./fpaste {};
@@ -26,11 +30,18 @@ in
   open-stage-control = callPackage ./open-stage-control {};
   petty = callPackage ./petty {};
   pridecat = callPackage ./pridecat {};
-  rofi-pass-wayland = callPackage ./rofi-pass-wayland { inherit (outpkgs) rofi-wayland; };
+  rofi-pass-wayland = callPackage ./rofi-pass-wayland {
+    rofi-wayland = if isOverlay then outpkgs.rofi-wayland else rofi-wayland;
+  };
   rofi-wayland = callPackage ./rofi-wayland {};
-  sonic-pi = libsForQt5.callPackage ./sonic-pi {};
   sonic-pi-tool = python3Packages.callPackage ./sonic-pi-tool {};
   swaynag-battery = callPackage ./swaynag-battery {};
+
+  sonic-pi = libsForQt5.callPackage ./sonic-pi {};
+  vmpk = libsForQt5.callPackage ./vmpk {
+    drumstick = if isOverlay then outpkgs.libsForQt5.drumstick else libsForQt5.drumstick;
+    inherit (pkgs) vmpk;
+  };
 
   monofur-nerdfont = nerdfonts.override {
     fonts = [ "Monofur" ];
@@ -38,9 +49,12 @@ in
 
   pass-wayland-otp = pass-wayland.withExtensions (ext: [ ext.pass-otp ]);
 } // (if isOverlay then {
-  inherit python3Packages vimPlugins;
+  inherit python3Packages libsForQt5 vimPlugins;
 } else {
   python3Packages = recurseIntoAttrs (callPackage ./python-modules {});
+  libsForQt5 = recurseIntoAttrs {
+    drumstick = pkgs.libsForQt5.callPackage ./drumstick { inherit (pkgs.libsForQt5) drumstick; };
+  };
   vimPlugins = recurseIntoAttrs (callPackage ./vim-plugins {});
 }) // (lib.optionalAttrs allowUnfree {
   ndi = callPackage ./ndi {
