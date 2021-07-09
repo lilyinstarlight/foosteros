72e4fe0872c008d644f7c0b44a03e88758a93064
Lily Foster <lily@lily.flowers>
2021-07-09 11:47:12 -0400

    python3Packages.oscpy: 0.5.0 -> 0.6.0

--- pkgs/python-modules/oscpy/default.nix
+++ pkgs/python-modules/oscpy/default.nix
@@ -2,11 +2,11 @@
 
 python3Packages.buildPythonPackage rec {
   pname = "oscpy";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = python3Packages.fetchPypi {
     inherit pname version;
-    sha256 = "22d4113accd9860e070a974ab8bbc024a9e4d2963a013e6b3a0699b6882ba421";
+    sha256 = "0cvdzdrmg46lg2dvf5j48ypddm8i94w3s1ih8vbcjck74sksaa07";
   };
 
   meta = with pkgs.lib; {
