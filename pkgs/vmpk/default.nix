f184bec180c1ad320a8efc2c29fa45ea1cc3727c
Lily Foster <lily@lily.flowers>
2021-08-10 12:55:39 -0400

    vmpk: 0.8.3 -> 0.8.4

--- pkgs/vmpk/default.nix
+++ pkgs/vmpk/default.nix
@@ -1,11 +1,11 @@
 { fetchurl, drumstick, qtx11extras, vmpk }:
 
 vmpk.overrideAttrs (attrs: rec {
-  version = "0.8.3";
+  version = "0.8.4";
 
   src = fetchurl {
     url = "mirror://sourceforge/${attrs.pname}/${version}/${attrs.pname}-${version}.tar.bz2";
-    sha256 = "0wk3jvlfdpd76vks4gdrhrv9m8icqbkimg5g3d5ybck3k01qaab6";
+    sha256 = "0kh8pns9pla9c47y2nwckjpiihczg6rpg96aignsdsd7vkql69s9";
   };
 
   buildInputs = [ drumstick qtx11extras ];
