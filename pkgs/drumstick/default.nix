1c8a8e0691b85ca3154b2d63996c8f66bcb891e4
Lily Foster <lily@lily.flowers>
2021-08-10 12:55:30 -0400

    libsForQt5.drumstick: 2.3.0 -> 2.3.1

--- pkgs/drumstick/default.nix
+++ pkgs/drumstick/default.nix
@@ -1,10 +1,10 @@
 { fetchurl, drumstick }:
 
 drumstick.overrideAttrs (attrs: rec {
-  version = "2.3.0";
+  version = "2.3.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/${attrs.pname}/${version}/${attrs.pname}-${version}.tar.bz2";
-    sha256 = "12haksnf91ra5w5dwnlc3rcw4js8wj4hsl6kzyqrx4q4fnpvjahk";
+    sha256 = "1rs248pkgn6d29nkvw9ab6dvi1vsz220jdmz1ddzr29cpyc0adfh";
   };
 })
