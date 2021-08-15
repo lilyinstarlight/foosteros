06d38034cb0b062c0998c167e091f95b9a810b29
Lily Foster <lily@lily.flowers>
2021-08-15 08:42:09 -0400

    tests/config: refactor to use testSystem function

--- tests/config.nix
+++ tests/config.nix
@@ -1,17 +1,16 @@
 { pkgs, outputs, ... }:
 
-{
-  minimal = (outputs.lib.baseSystem {
+let
+  testSystem = configuration: (outputs.lib.baseSystem {
     modules = [
       { nixpkgs.config.allowUnfree = false; }
-      ../hosts/minimal/configuration.nix
+      configuration
     ];
   }).config.system.build.toplevel;
+in
 
-  bina = (outputs.lib.baseSystem {
-    modules = [
-      { nixpkgs.config.allowUnfree = false; }
-      ../hosts/bina/configuration.nix
-    ];
-  }).config.system.build.toplevel;
+{
+  minimal = testSystem ../hosts/minimal/configuration.nix;
+
+  bina = testSystem ../hosts/bina/configuration.nix;
 }
