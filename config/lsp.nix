bcb51f86d119aa564db075f6da60d895ae08c65e
Lily Foster <lily@lily.flowers>
2021-09-30 11:44:40 -0400

    config/lsp: rename module

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     python3Packages.python-lsp-server
-    rust-analyzer rustc cargo
+    rust-analyzer rustc cargo clippy
     rnix-lsp
     nodePackages.bash-language-server
   ];
