2ee5afa030e112feb44a51b142142541fa169a8e
Lily Foster <lily@lily.flowers>
2021-09-30 07:50:38 -0400

    wtype: unstable-2021-08-17 -> unstable-2021-09-22

--- pkgs/wtype/default.nix
+++ pkgs/wtype/default.nix
@@ -1,13 +1,13 @@
 { fetchFromGitHub, wtype }:
 
 wtype.overrideAttrs (attrs: rec {
-  version = "unstable-2021-08-17";
+  version = "unstable-2021-09-22";
 
   src = fetchFromGitHub {
     owner = "atx";
     repo = "wtype";
     #rev = "v${version}";
-    rev = "6536edfb917ed008c99b6cdd8d78c911782cb8e1";
-    sha256 = "10pshf98x0ljs20h911vyjwrr06sgqcrcshhx59crf68sxdr0fry";
+    rev = "707c5febee1cf6ec681d62800cbacbf1dd7a09e5";
+    sha256 = "sha256-Pj/6RGT/Bxu6WAZZpn2lpb3qYmvnZqPiQ8Z/d4Kd4AM=";
   };
 })
