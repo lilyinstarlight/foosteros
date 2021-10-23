431892e90cf1770952dc6639321f495c4f7b28b6
Lily Foster <lily@lily.flowers>
2021-10-23 10:20:47 -0400

    sonic-pi-beta: bump to latest dev commit

--- pkgs/sonic-pi-beta/default.nix
+++ pkgs/sonic-pi-beta/default.nix
@@ -37,8 +37,8 @@ stdenv.mkDerivation rec {
     owner = "sonic-pi-net";
     repo = pname;
     #rev = "v${version}";
-    rev = "710107fe22c5977b9fa5e83b71e30f847610e240";
-    sha256 = "sha256-UOqkGv99HYZqRME94L/hfDn/uPWJFZ0zVaQTBJKoZJw=";
+    rev = "38fd1738c520bf10ee0ff3a9d1a1da231aa948c9";
+    sha256 = "sha256-ff4xGDTPbzQFn6efzO1U3ZYVtmIFbxy+DCAwPTfAaAw=";
   };
 
   patches = [
--- pkgs/sonic-pi-beta/generate-mix-dependencies.sh
+++ pkgs/sonic-pi-beta/generate-mix-dependencies.sh
@@ -3,7 +3,7 @@
 
 # Download mix.lock for the tau server from the release
 #curl https://raw.githubusercontent.com/sonic-pi-net/sonic-pi/v4.0.0/app/server/beam/tau/mix.lock >mix.lock
-curl https://raw.githubusercontent.com/sonic-pi-net/sonic-pi/710107fe22c5977b9fa5e83b71e30f847610e240/app/server/beam/tau/mix.lock >mix.lock
+curl https://raw.githubusercontent.com/sonic-pi-net/sonic-pi/38fd1738c520bf10ee0ff3a9d1a1da231aa948c9/app/server/beam/tau/mix.lock >mix.lock
 
 mix2nix mix.lock >mix-deps.nix
 
