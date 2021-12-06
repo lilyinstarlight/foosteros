3fb0758a70b46fae15a476115a37abc7e62e4f59
Lily Foster <lily@lily.flowers>
2021-12-06 09:46:06 -0500

    fooster-materia-theme: switch from resvg back to inkscape

--- pkgs/materia-theme/default.nix
+++ pkgs/materia-theme/default.nix
@@ -1,7 +1,7 @@
-{ lib, materia-theme, bc, resvg, optipng, gnome }:
+{ lib, materia-theme, bc, inkscape, optipng, gnused, gnome }:
 
 materia-theme.overrideAttrs (attrs: rec {
-  nativeBuildInputs = attrs.nativeBuildInputs ++ [ bc resvg optipng ];
+  nativeBuildInputs = attrs.nativeBuildInputs ++ [ bc inkscape optipng gnused ];
 
   propagatedUserEnvPkgs = attrs.propagatedUserEnvPkgs ++ [
     gnome.gnome-themes-extra  # apparently needed for gtk2 style (at least as used by QGtkStyle)
@@ -14,10 +14,6 @@ materia-theme.overrideAttrs (attrs: rec {
     sed -i install.sh \
       -e "s|if .*which gnome-shell.*;|if true;|" \
       -e "s|CURRENT_GS_VERSION=.*$|CURRENT_GS_VERSION=${lib.versions.majorMinor gnome.gnome-shell.version}|"
-    for script in render-assets.sh src/*/render-asset.sh; do
-      sed -i "$script" \
-        -e "s|\brendersvg\b|resvg|g"
-    done
     ./change_color.sh -t $out/share/themes -o Materia-Fooster <(echo -e "MATERIA_COLOR_VARIANT=dark\nSEL_BG=F29BD4\nFG=EEEEEE\nBG=181818\nHDR_BG=2d2d2d\nHDR_FG=EEEEEE\nMATERIA_SURFACE=343434\nMATERIA_VIEW=242424\n")
   '';
 
