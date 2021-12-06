15062f232a729f0d84f30f7391f446fac2dea150
Lily Foster <lily@lily.flowers>
2021-12-06 11:38:12 -0500

    fooster-materia-theme: make resvg work again

--- pkgs/materia-theme/default.nix
+++ pkgs/materia-theme/default.nix
@@ -1,7 +1,7 @@
-{ lib, materia-theme, bc, inkscape, optipng, gnused, gnome }:
+{ lib, materia-theme, bc, resvg, optipng, gnused, gtk4, gnome, util-linux }:
 
 materia-theme.overrideAttrs (attrs: rec {
-  nativeBuildInputs = attrs.nativeBuildInputs ++ [ bc inkscape optipng gnused ];
+  nativeBuildInputs = attrs.nativeBuildInputs ++ [ bc resvg optipng gnused gtk4 gnome.gnome-shell util-linux ];
 
   propagatedUserEnvPkgs = attrs.propagatedUserEnvPkgs ++ [
     gnome.gnome-themes-extra  # apparently needed for gtk2 style (at least as used by QGtkStyle)
@@ -10,11 +10,22 @@ materia-theme.overrideAttrs (attrs: rec {
   dontConfigure = true;
 
   installPhase = ''
+    # fix shebangs
     patchShebangs change_color.sh install.sh parse-sass.sh render-assets.sh scripts/*.sh src/*/render-assets.sh src/*/render-asset.sh
-    sed -i install.sh \
-      -e "s|if .*which gnome-shell.*;|if true;|" \
-      -e "s|CURRENT_GS_VERSION=.*$|CURRENT_GS_VERSION=${lib.versions.majorMinor gnome.gnome-shell.version}|"
-    ./change_color.sh -t $out/share/themes -o Materia-Fooster <(echo -e "MATERIA_COLOR_VARIANT=dark\nSEL_BG=F29BD4\nFG=EEEEEE\nBG=181818\nHDR_BG=2d2d2d\nHDR_FG=EEEEEE\nMATERIA_SURFACE=343434\nMATERIA_VIEW=242424\n")
+
+    # fix resvg command name
+    for script in render-assets.sh src/*/render-asset.sh; do
+      sed -i "$script" \
+        -e "s|\brendersvg\b|resvg|g"
+    done
+
+    # remove nonexistent asset references for building
+    sed -i src/gtk-2.0/assets.txt \
+      -e '/^handle-horz-/d' \
+      -e '/^handle-vert-/d'
+
+    # make new color scheme
+    ./change_color.sh -i false -t $out/share/themes -o Materia-Fooster <(echo -e "MATERIA_COLOR_VARIANT=dark\nSEL_BG=F29BD4\nFG=EEEEEE\nBG=181818\nHDR_BG=2d2d2d\nHDR_FG=EEEEEE\nMATERIA_SURFACE=343434\nMATERIA_VIEW=242424\n")
   '';
 
   meta = with lib; attrs.meta // {
