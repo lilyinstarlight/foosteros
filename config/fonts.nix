2d3363f87c77b474b7901c8148e70665c3f411ae
Lily Foster <lily@lily.flowers>
2022-01-20 18:49:36 -0500

    config/fonts: noto-fonts-cjk -> noto-fonts-cjk-sans

--- config/fonts.nix
+++ config/fonts.nix
@@ -3,7 +3,7 @@
 {
   fonts.fonts = with pkgs; [
     aileron
-    noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji
+    noto-fonts noto-fonts-extra noto-fonts-cjk-sans noto-fonts-emoji
     dejavu_fonts
     freefont_ttf
     gyre-fonts
