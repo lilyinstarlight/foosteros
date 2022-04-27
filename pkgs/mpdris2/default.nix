122f33cbc2851ab18a0bc6733e91c153a4da7ddf
Lily Foster <lily@lily.flowers>
2022-04-27 07:09:33 -0400

    mpdris2: update patches for testing

--- pkgs/mpdris2/default.nix
+++ pkgs/mpdris2/default.nix
@@ -1,17 +1,17 @@
 { lib, fetchFromGitHub, mpdris2 }:
 
 mpdris2.overrideAttrs (attrs: rec {
-  version = "0.9.1";
+  version = "unstable-2022-04-26";
 
   src = fetchFromGitHub {
     owner = "eonpatapon";
     repo = attrs.pname;
-    rev = version;
-    hash = "sha256-1Y6K3z8afUXeKhZzeiaEF3yqU0Ef7qdAj9vAkRlD2p8=";
+    rev = "8bf0ae8fc67eb7ad1b7c7d94191eddfcd10c38a8";
+    hash = "sha256-3zO93gU9F9aldyNnfaphtCAZY6WqSgmQ0fin9WkHsII=";
   };
 
   patches = (if lib.hasAttr "patches" attrs then attrs.patches else []) ++ [
-    ./mpdris2-mp4-cover-art.patch
+    ./mpdris2-cover-art-detection-loop-fix.patch
     ./mpdris2-mopidy-cover-art.patch
   ];
 })
