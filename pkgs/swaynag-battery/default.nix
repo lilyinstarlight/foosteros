61ab3441171f71bf0fcd96f888e69ab8f0b1c60e
Lily Foster <lily@lily.flowers>
2022-05-08 08:15:32 -0400

    swaynag-battery: unstable-2022-04-30 -> unstable-2022-05-07

--- pkgs/swaynag-battery/default.nix
+++ pkgs/swaynag-battery/default.nix
@@ -3,24 +3,16 @@
 let swaynag-battery =
 buildGoModule rec {
   pname = "swaynag-battery";
-  version = "unstable-2022-04-30";
+  version = "unstable-2022-05-07";
 
   src = fetchFromGitHub {
     owner = "m00qek";
     repo = pname;
     #rev = "v${version}";
-    rev = "ba9f3594e1f051a26bb9964240adc31b90327f9e";
-    hash = "sha256-ARkXU0zOcptMlo5947oT0KZvARFNU1DBIEYdue2FzfY=";
+    rev = "fb413593363ec5fc9bfe508cf28dfb8ce95707f3";
+    hash = "sha256-TY5rUezGmkD4VYAvHpHhRm91rzxd7hFEgSSw8io7DuM=";
   };
   vendorSha256 = "sha256-h9Zj3zmQ0Xpili5Pl6CXh1L0bb2uL1//B79I4/ron08=";
-
-  patches = [
-    (fetchpatch {
-      name = "swaynag-fix-minor-typo.patch";
-      url = "https://github.com/m00qek/swaynag-battery/commit/75250871eab89a06e6623afc608927b996211711.patch";
-      hash = "sha256-q0Jb9WD6lCP3hg7SGYOO0wp+ucw5xNA93ib7d3qc5o4=";
-    })
-  ];
 
   nativeBuildInputs = [
     makeWrapper
