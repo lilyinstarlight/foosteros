eb663c36cdb6521e3e063f1c05cb71f2a2958ec6
Lily Foster <lily@lily.flowers>
2022-05-10 07:30:15 -0400

    playdate-sdk: 1.10.0 -> 1.11.0

--- pkgs/playdate-sdk/default.nix
+++ pkgs/playdate-sdk/default.nix
@@ -18,11 +18,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "playdate-sdk";
-  version = "1.10.0";
+  version = "1.11.0";
 
   src = fetchurl {
     url = "https://download.panic.com/playdate_sdk/Linux/PlaydateSDK-${version}.tar.gz";
-    hash = "sha256-aBk5aVfk2iUj53h1acOzOtQdltFG+v5AupyzFX2LPKQ=";
+    hash = "sha256-ShoS2g0oAdeuSsKzsxsGfDLIOPeDSfU8c+4QyVJ35qU=";
   };
 
   nativeBuildInputs = [
@@ -71,7 +71,32 @@ stdenv.mkDerivation rec {
 
     # Helper scripts
     mkdir -p $out/libexec
-    cp ${./PlaydateSimulator.sh} $out/libexec/PlaydateSimulator
+    cat >$out/libexec/PlaydateSimulator <<EOF
+    #!/bin/sh
+    if [ ! -d "\$HOME/.Playdate Simulator/sdk" ]; then
+        echo "Creating SDK and virtual disk in \$HOME/.Playdate Simluator/sdk..."
+        mkdir -p "\$HOME/.Playdate Simulator/sdk"
+        for dir in $out/sdk/*; do
+            if [ "\$(basename "\$dir")" != "Disk" ]; then
+                ln -s "\$dir" "\$HOME/.Playdate Simulator/sdk/\$(basename "\$dir")"
+            else
+                cp -r "\$dir" "\$HOME/.Playdate Simulator/sdk/\$(basename "\$dir")"
+                chmod -R u=rwX,g=rX,o=rX "\$HOME/.Playdate Simulator/sdk/\$(basename "\$dir")"
+            fi
+        done
+    fi
+
+    echo "Setting SDK path to \$HOME/.Playdate Simluator/sdk..."
+    if grep -qs "^SDKDirectory=" "\$HOME/.Playdate Simulator/Playdate Simulator.ini"; then
+        sed -i -e "s#^SDKDirectory=.*\\\$#SDKDirectory=\$HOME/.Playdate Simulator/sdk#" "\$HOME/.Playdate Simulator/Playdate Simulator.ini"
+    else
+        echo >"\$HOME/.Playdate Simulator/Playdate Simulator.ini"
+        sed -i -e "1iSDKDirectory=\$HOME/.Playdate Simulator/sdk\n[LastUsed]\nPDXDirectory=\$HOME/.Playdate Simulator/sdk/Disk/System/Launcher.pdx/" "\$HOME/.Playdate Simulator/Playdate Simulator.ini"
+    fi
+
+    exec $out/sdk/bin/PlaydateSimulator "\$@"
+    EOF
+    chmod +x $out/libexec/PlaydateSimulator
 
     # Executables
     makeWrapper $out/sdk/bin/pdc $out/bin/pdc \
