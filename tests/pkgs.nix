b31b9d8791c4f69856d8cc885c6a831af9ff98f9
Lily Foster <lily@lily.flowers>
2022-05-11 06:25:50 -0400

    tests: remove unused let statement

--- tests/pkgs.nix
+++ tests/pkgs.nix
@@ -2,10 +2,6 @@
 
 with pkgs;
 
-let
-  ifSupported = drv: test: if drv.meta.unsupported then "skip" else test;
-in
-
 lib.listToAttrs (lib.flatten (
   map (drv: if drv ? tests then lib.mapAttrsToList (
       name: value: { name = "pkg-test-" + (if drv ? pname then drv.pname else drv.name) + "-" + name; inherit value; }
