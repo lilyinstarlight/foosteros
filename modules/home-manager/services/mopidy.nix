0562c4d3fb4e48cae95d84dbd8470f94d21a92f2
Lily Foster <lily@lily.flowers>
2022-05-24 11:42:49 -0400

    hm/services/mopidy: extend current mopidy module in home-manager for extra config files

--- modules/home-manager/services/mopidy.nix
+++ modules/home-manager/services/mopidy.nix
@@ -3,88 +3,39 @@
 with lib;
 
 let
+
   cfg = config.services.mopidy;
 
-  iniFormat = pkgs.formats.ini {};
-
   mopidyEnv = pkgs.buildEnv {
-    name = "mopidy-with-extensions-${cfg.package.version}";
+    name = "mopidy-with-extensions-${pkgs.mopidy.version}";
     paths = closePropagation cfg.extensionPackages;
     pathsToLink = [ "/${pkgs.mopidyPackages.python.sitePackages}" ];
     buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
-      makeWrapper ${cfg.package}/bin/mopidy $out/bin/mopidy \
+      makeWrapper ${pkgs.mopidy}/bin/mopidy $out/bin/mopidy \
         --prefix PYTHONPATH : $out/${pkgs.mopidyPackages.python.sitePackages}
     '';
   };
+
+  configFilePaths = concatStringsSep ":"
+    ([ "${config.xdg.configHome}/mopidy/mopidy.conf" ] ++ cfg.extraConfigFiles);
+
 in {
   options.services.mopidy = {
-    enable = mkEnableOption "Mopidy, a music player daemon";
-
-    package = mkOption {
-      type = types.package;
-      default = pkgs.mopidy;
-      defaultText = literalExpression "pkgs.mopidy";
-      description = "The Mopidy package to use.";
-    };
-
-    extensionPackages = mkOption {
-      default = [];
-      type = types.listOf types.package;
-      example = literalExpression "[ pkgs.mopidy-spotify ]";
-      description = ''
-        Mopidy extensions that should be loaded by the service.
-      '';
-    };
-
-    settings = mkOption {
-      type = iniFormat.type;
-      default = {};
-      description = ''
-        Configuration written to
-        <filename>~/.config/mopidy/mopidy.conf</filename>
-      '';
-    };
-
     extraConfigFiles = mkOption {
-      default = [];
-      type = types.listOf types.str;
+      default = [ ];
+      type = types.listOf types.path;
       description = ''
-        Extra config file read by Mopidy when the service starts.
-        Later files in the list overrides earlier configuration
-        and structured settings.
+        Extra configuration files read by Mopidy when the service starts.
+        Later files in the list override earlier configuration files and
+        structured settings.
       '';
     };
   };
 
   config = mkIf cfg.enable {
-    systemd.user.services.mopidy = {
-      Install = { WantedBy = [ "default.target" ]; };
-
-      Unit = {
-        Description = "Mopidy music player daemon";
-        After = [ "network.target" "sound.target" ];
-      };
-
-      Service = {
-        Type = "simple";
-        Restart = "on-failure";
-        RestartSec = "5s";
-        ExecStart = "${mopidyEnv}/bin/mopidy --config ${concatStringsSep ":" (["${config.xdg.configHome}/mopidy/mopidy.conf"] ++ cfg.extraConfigFiles)}";
-      };
-    };
-
-    systemd.user.services.mopidy-scan = {
-      Unit = {
-        Description = "Mopidy local files scanner";
-      };
-
-      Service = {
-        Type = "oneshot";
-        ExecStart = "${mopidyEnv}/bin/mopidy --config ${concatStringsSep ":" (["${config.xdg.configHome}/mopidy/mopidy.conf"] ++ cfg.extraConfigFiles)} local scan";
-      };
-    };
-
-    xdg.configFile."mopidy/mopidy.conf".source = iniFormat.generate "mopidy.conf" cfg.settings;
+    # TODO: remove this entire module once https://github.com/nix-community/home-manager/pull/2970 is merged
+    systemd.user.services.mopidy.Service.ExecStart = lib.mkForce "${mopidyEnv}/bin/mopidy --config ${configFilePaths}";
+    systemd.user.services.mopidy-scan.Service.ExecStart = lib.mkForce "${mopidyEnv}/bin/mopidy --config ${configFilePaths} local scan";
   };
 }
