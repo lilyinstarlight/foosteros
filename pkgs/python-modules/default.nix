9741d72e50a4cf41b9413e3be56c5968ae840e99
Lily Foster <lily@lily.flowers>
2022-08-04 19:26:35 -0400

    pkgs: remove merged python-lsp-server fix

--- pkgs/python-modules/default.nix
+++
@@ -1,9 +0,0 @@
-{ python3Packages }:
-
-# TODO: remove this whole directory when NixOS/nixpkgs#183862 is merged
-rec {
-  whatthepatch = python3Packages.callPackage ./whatthepatch {};
-  python-lsp-server = python3Packages.callPackage ./python-lsp-server {
-    inherit whatthepatch;
-  };
-}
