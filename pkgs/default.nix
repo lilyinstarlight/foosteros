a81b28a9448683673baa8604d263831da451d9a1
Lily Foster <lily@lily.flowers>
2022-08-10 20:49:39 -0400

    pkgs: removed merged packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -42,17 +42,6 @@ in
       platforms = platforms.linux;
     };
   });
-
-  # TODO: remove when NixOS/nixpkgs#185796 is merged
-  nix-bisect = nix-bisect.overrideAttrs (attrs: {
-    patches = attrs.patches ++ [
-      (fetchpatch {
-        # Fixes BuildFailure drvs_failed inconsistency
-        url = "https://github.com/timokau/nix-bisect/commit/9f3a17783046baae64c16f9e2be917c2603977fc.patch";
-        hash = "sha256-U9NUtgwslcgIf/wvH/WE7t0HGs2OP3wvYDKrb5j+lp0=";
-      })
-    ];
-  });
 } // (if isOverlay then {
   inherit vimPlugins;
 } else {
@@ -70,13 +59,6 @@ in
 
   # unfree packages
   playdate-sdk = callPackage ./playdate-sdk {};
-
-  # TODO: remove when NixOS/nixpkgs#185794 is merged
-  kodelife = callPackage ./kodelife {
-    inherit (gst_all_1) gstreamer gst-plugins-base;
-  };
-  # TODO: remove when NixOS/nixpkgs#185795 is merged
-  touchosc = callPackage ./touchosc {};
 });
 
 in mypkgs
