227f2e392b2797784457175d2168fadd7b8d2a58
Lily Foster <lily@lily.flowers>
2022-08-18 15:25:24 -0400

    nixos/programs/sway: run xdg autostart for fcitx5

--- modules/nixos/programs/sway.nix
+++ modules/nixos/programs/sway.nix
@@ -12,19 +12,18 @@ in
       ### systemd integration
       exec "systemctl --user import-environment XDG_SESSION_CLASS XDG_CONFIG_DIRS XDG_DATA_DIRS XDG_SESSION_DESKTOP XDG_CURRENT_DESKTOP XDG_SESSION_TYPE DCONF_PROFILE XDG_DESKTOP_PORTAL_DIR DISPLAY WAYLAND_DISPLAY SWAYSOCK NIXOS_OZONE_WL"
       exec "command -v dbus-update-activation-environment >/dev/null 2>&1 && dbus-update-activation-environment --systemd XDG_SESSION_CLASS XDG_CONFIG_DIRS XDG_DATA_DIRS XDG_SESSION_DESKTOP XDG_CURRENT_DESKTOP XDG_SESSION_TYPE DCONF_PROFILE XDG_DESKTOP_PORTAL_DIR DISPLAY WAYLAND_DISPLAY SWAYSOCK NIXOS_OZONE_WL"
-      exec "systemctl --user start wlr-session.target"
+      exec "systemctl --user restart wlr-session.target"
 
       include /etc/sway/config.d/*
     '';
 
     systemd.user.targets.wlr-session = {
-      unitConfig = {
-        Description = "wlroots compositor session";
-        Documentation = "man:systemd.special(7)";
-        BindsTo = "graphical-session.target";
-        Wants = "graphical-session-pre.target";
-        After = "graphical-session-pre.target";
-      };
+      description = "wlroots compositor session";
+
+      bindsTo = [ "graphical-session.target" ];
+      wants = [ "graphical-session-pre.target" "xdg-desktop-autostart.target" ];
+      after = [ "graphical-session-pre.target" ];
+      before = [ "xdg-desktop-autostart.target" ];
     };
   };
 }
