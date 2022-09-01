173ac74d1d8af2d387d622d438b2efd9835d2366
Lily Foster <lily@lily.flowers>
2022-09-01 13:17:37 -0400

    home-manager/mpdris2,hosts/bina: change to simple override of home-manager module

--- modules/home-manager/services/mpdris2.nix
+++ modules/home-manager/services/mpdris2.nix
@@ -21,6 +21,8 @@ let
       host = cfg.mpd.host;
       port = cfg.mpd.port;
       music_dir = cfg.mpd.musicDirectory;
+    } // optionalAttrs (cfg.mpd.password != null) {
+      password = cfg.mpd.password;
     };
 
     Bling = {
@@ -31,82 +33,15 @@ let
   };
 
 in {
-  disabledModules = [ "services/mpdris2.nix" ];
-
   options.services.mpdris2 = {
-    enable = mkEnableOption "mpDris2 the MPD to MPRIS2 bridge";
-    notifications = mkEnableOption "song change notifications";
-    multimediaKeys = mkEnableOption "multimedia key support";
     cdPrevious = mkEnableOption "CD-like previous command";
-
-    package = mkOption {
-      type = types.package;
-      default = pkgs.mpdris2;
-      defaultText = literalExpression "pkgs.mpdris2";
-      description = "The mpDris2 package to use.";
-    };
-
-    mpd = {
-      host = mkOption {
-        type = types.str;
-        default = config.services.mpd.network.listenAddress;
-        defaultText = "config.services.mpd.network.listenAddress";
-        example = "192.168.1.1";
-        description = "The address where MPD is listening for connections.";
-      };
-
-      port = mkOption {
-        type = types.port;
-        default = config.services.mpd.network.port;
-        defaultText = "config.services.mpd.network.port";
-        description = ''
-          The port number where MPD is listening for connections.
-        '';
-      };
-
-      musicDirectory = mkOption {
-        type = types.nullOr types.path;
-        default = config.services.mpd.musicDirectory;
-        defaultText = "config.services.mpd.musicDirectory";
-        description = ''
-          If set, mpDris2 will use this directory to access music artwork.
-        '';
-      };
-
-      service = mkOption {
-        type = types.nullOr types.str;
-        default = "mpd.service";
-        description = ''
-          The systemd service name for MPD.
-        '';
-      };
-    };
   };
 
   config = mkIf cfg.enable {
-    assertions = [{
-      assertion = hasAttr (removeSuffix ".service" cfg.mpd.service) config.systemd.user.services;
-      message = "The mpdris2 module requires 'services.mpd.enable = true' or setting 'services.mpdris2.mpd.service' to a systemd service name.";
-    }];
-
     xdg.configFile."mpDris2/mpDris2.conf".text = toIni mpdris2Conf;
 
     systemd.user.services.mpdris2 = {
-      Install = { WantedBy = [ "graphical-session.target" ]; };
-
-      Unit = {
-        Description = "MPRIS 2 support for MPD";
-        PartOf = [ "graphical-session.target" ];
-        After = [ cfg.mpd.service ];
-      };
-
-      Service = {
-        Type = "dbus";
-        Restart = "on-failure";
-        RestartSec = "5s";
-        ExecStart = "${cfg.package}/bin/mpDris2";
-        BusName = "org.mpris.MediaPlayer2.mpd";
-      };
+      Service.Type = mkOverride 75 "dbus";  # 50 is force prio and 100 is default prio
     };
   };
 }
