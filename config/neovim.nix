4bcc0f7a121771b003713e3c96c3da35fc82a4ff
Lily Foster <lily@lily.flowers>
2022-10-30 12:08:45 -0400

    config/neovim: nit spacing

--- config/neovim.nix
+++ config/neovim.nix
@@ -121,7 +121,7 @@
 
         local nvim_lsp = require('lspconfig')
 
-        local on_attach = function(client, bufnr)
+        local on_attach = function (client, bufnr)
           local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
           local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
