7362178546ef2908df6db0e6211182c092a1cb68
Lily Foster <lily@lily.flowers>
2022-11-18 09:57:59 -0500

    crank: add new updateScript

--- pkgs/crank/default.nix
+++ pkgs/crank/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, rustPlatform, fetchFromGitHub, makeWrapper, rustNightlyToolchain, gcc-arm-embedded, playdate-sdk, xdg-utils, unstableGitUpdater }:
+{ lib, stdenv, rustPlatform, fetchFromGitHub, makeWrapper, rustNightlyToolchain, gcc-arm-embedded, playdate-sdk, xdg-utils, writeScript, unstableGitUpdater }:
 
 rustPlatform.buildRustPackage rec {
   pname = "crank";
@@ -26,7 +26,57 @@ rustPlatform.buildRustPackage rec {
       --set PLAYDATE_SDK_PATH '${playdate-sdk}/sdk'
   '';
 
-  passthru.updateScript = unstableGitUpdater {};
+  passthru.updateScript = writeScript "update-crank-unstable.sh" ''
+    #!/usr/bin/env nix-shell
+    #!nix-shell -i bash -p nix jq git
+
+    set -euo pipefail
+
+    nixpkgs="$(git rev-parse --show-toplevel || (printf 'Could not find root of nixpkgs repo\nAre we running from within the nixpkgs git repo?\n' >&2; exit 1))"
+
+    stripwhitespace() {
+        sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
+    }
+
+    nixeval() {
+        nix --extra-experimental-features nix-command eval --json --impure -f "$nixpkgs" "$1" | jq -r .
+    }
+
+    vendorhash() {
+        (nix --extra-experimental-features nix-command build --impure -f "$nixpkgs" --no-link "$1" 2>&1 >/dev/null | tail -n3 | grep -F got: | cut -d: -f2- | stripwhitespace) 2>/dev/null || true
+    }
+
+    findpath() {
+        path="$(nix --extra-experimental-features nix-command eval --json --impure -f "$nixpkgs" "$1.meta.position" | jq -r . | cut -d: -f1)"
+        outpath="$(nix --extra-experimental-features nix-command eval --json --impure --expr "builtins.fetchGit \"$nixpkgs\"")"
+
+        if [ -n "$outpath" ]; then
+            path="''${path/$(echo "$outpath" | jq -r .)/$nixpkgs}"
+        fi
+
+        echo "$path"
+    }
+
+    ${lib.escapeShellArgs (unstableGitUpdater {})}
+
+    attr="''${UPDATE_NIX_ATTR_PATH:-crank}"
+
+    pkgpath="$(findpath "$attr")"
+
+    if git diff --exit-code --quiet "$pkgpath"; then
+        echo 'update-crank-unstable.sh: Package version not updated, nothing to do.'
+        exit 0
+    fi
+
+    curhash="$(nixeval "$attr.cargoDeps.outputHash")"
+    newhash="$(vendorhash "$attr.cargoDeps")"
+
+    if [ -n "$newhash" ] && [ "$curhash" != "$newhash" ]; then
+        sed -i -e "s|\"$curhash\"|\"$newhash\"|" "$pkgpath"
+    else
+        echo 'update-crank-unstable.sh: New vendorHash same as old vendorHash, nothing to do.'
+    fi
+  '';
 
   meta = with lib; {
     description = "A cargo wrapper for creating games for the Playdate handheld gaming system";
