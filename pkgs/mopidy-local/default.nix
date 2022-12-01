d3a153a370f507e1e98d2088754085f71ef27b53
Lily Foster <lily@lily.flowers>
2022-12-01 18:49:43 -0500

    mopidy-local: fix platforms

--- pkgs/mopidy-local/default.nix
+++ pkgs/mopidy-local/default.nix
@@ -4,4 +4,8 @@ mopidy-local.overrideAttrs (attrs: {
   patches = (attrs.patches or []) ++ [
     ./mopidy-tests-3.4.0.patch
   ];
+
+  meta = with lib; (attrs.meta or {}) // {
+    platforms = platforms.linux;
+  };
 })
