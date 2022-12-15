689e16cb2e202b08edd32848014dcd48170cc903
Lily Foster <lily@lily.flowers>
2022-12-15 10:16:01 -0500

    config/installer: add prepare script and install hostname to iso filename

--- config/installer.nix
+++ config/installer.nix
@@ -10,23 +10,25 @@
   # TODO: installer does not support systemd initrd yet
   boot.initrd.systemd.enable = lib.mkImageMediaOverride false;
 
-  isoImage.isoName = lib.mkForce "foosteros.iso";
+  isoImage.isoName = lib.mkForce "foosteros-${config.system.build.installHostname}.iso";
 
   environment.systemPackages = with pkgs; [
     (writeShellApplication {
       name = "foosteros-install";
       runtimeInputs = [ nix openssh git ];
       text = ''
-        hostname="${config.system.build.installHostname}"
+        export INSTALL_HOSTNAME="${config.system.build.installHostname}"
+
+        which foosteros-prepare &>/dev/null && foosteros-prepare
 
         ${config.system.build.installDisko}
 
         mkdir -p /mnt/etc
         git clone https://github.com/lilyinstarlight/foosteros.git /mnt/etc/nixos
 
-        if nix eval /mnt/etc/nixos#nixosConfigurations."$hostname".config.environment.persistenence./state >/dev/null; then
+        if nix eval /mnt/etc/nixos#nixosConfigurations."$INSTALL_HOSTNAME".config.environment.persistenence./state >/dev/null; then
           mkdir -p /mnt/etc/ssh
-          echo "Please enter the SSH host key for $hostname and then press CTRL-D:"
+          echo "Please enter the SSH host key for $INSTALL_HOSTNAME and then press CTRL-D:"
           cat >/mnt/etc/ssh/ssh_host_rsa_key
           ssh-keygen -y -f /mnt/etc/ssh/ssh_host_rsa_key >/mnt/etc/ssh/ssh_host_rsa_key.pub
           chmod u=rw,go= /mnt/etc/ssh/ssh_host_rsa_key
@@ -39,11 +41,11 @@
 
         installArgs=(--no-channel-copy)
 
-        if [ "$(nix eval /mnt/etc/nixos#nixosConfigurations."$hostname".config.users.mutableUsers)" = "false" ]; then
+        if [ "$(nix eval /mnt/etc/nixos#nixosConfigurations."$INSTALL_HOSTNAME".config.users.mutableUsers)" = "false" ]; then
           installArgs+=(--no-root-password)
         fi
 
-        nixos-install --flake "/mnt/etc/nixos#$hostname" "''${installArgs[@]}"
+        nixos-install --flake "/mnt/etc/nixos#$INSTALL_HOSTNAME" "''${installArgs[@]}"
       '';
     })
   ];
