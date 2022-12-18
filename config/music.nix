4b2500d1556710b001c379691fb5995b9ab825ee
Lily Foster <lily@lily.flowers>
2022-12-18 12:42:05 -0500

    config/music: use pkgs.runtimeShell

--- config/music.nix
+++ config/music.nix
@@ -31,7 +31,7 @@
       # wait for mako since mpdris2 disables notifications if the org.freedesktop.Notifications busname is not available yet
       Unit.After = [ "mako.service" ];
       # wait for mpd port to become available to avoid reconnected notification on bootup
-      Service.ExecStartPre = "${pkgs.coreutils}/bin/timeout 60 ${pkgs.bash}/bin/sh -c 'while ! ${pkgs.iproute2}/sbin/ss -tlnH sport = :${toString cfg.services.mpdris2.mpd.port} | ${pkgs.gnugrep}/bin/grep -q \"^LISTEN.*:${toString cfg.services.mpdris2.mpd.port}\"; do ${pkgs.coreutils}/bin/sleep 1; done'";
+      Service.ExecStartPre = "${pkgs.coreutils}/bin/timeout 60 ${pkgs.runtimeShell} -c 'while ! ${pkgs.iproute2}/sbin/ss -tlnH sport = :${toString cfg.services.mpdris2.mpd.port} | ${pkgs.gnugrep}/bin/grep -q \"^LISTEN.*:${toString cfg.services.mpdris2.mpd.port}\"; do ${pkgs.coreutils}/bin/sleep 1; done'";
     };
 
     programs.beets = {
