c29506db241dd76d912d475066a7e7009b6c8aa5
Lily Foster <lily@lily.flowers>
2022-12-25 14:39:47 -0500

    config/music: use mopidy-notify

--- config/music.nix
+++ config/music.nix
@@ -15,23 +15,8 @@
         local.media_dir = "${cfg.home.homeDirectory}/music";
       };
       extensionPackages = with pkgs; [
-        mopidy-local mopidy-iris mopidy-mpd
+        mopidy-local mopidy-iris mopidy-mpris mopidy-notify mopidy-mpd
       ] ++ (lib.optionals config.nixpkgs.config.allowUnfree [ /*mopidy-spotify*/ ]);
-    };
-
-    services.mpdris2 = {
-      enable = true;
-      notifications = true;
-      multimediaKeys = false;
-      cdPrevious = true;
-      mpd.musicDirectory = cfg.services.mopidy.settings.local.media_dir;
-    };
-
-    systemd.user.services.mpdris2 = {
-      # wait for mako since mpdris2 disables notifications if the org.freedesktop.Notifications busname is not available yet
-      Unit.After = [ "mako.service" ];
-      # wait for mpd port to become available to avoid reconnected notification on bootup
-      Service.ExecStartPre = "${pkgs.coreutils}/bin/timeout 60 ${pkgs.runtimeShell} -c 'while ! ${pkgs.iproute2}/sbin/ss -tlnH sport = :${toString cfg.services.mpdris2.mpd.port} | ${pkgs.gnugrep}/bin/grep -q \"^LISTEN.*:${toString cfg.services.mpdris2.mpd.port}\"; do ${pkgs.coreutils}/bin/sleep 1; done'";
     };
 
     programs.beets = {
