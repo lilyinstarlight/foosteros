755ee9058985794a700e5ae94e5ab57415ba7d41
Lily Foster <lily@lily.flowers>
2023-01-03 16:24:32 -0500

    config/lsp: add rustfmt

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     (python3.withPackages (ps: with ps; [ python-lsp-server pylsp-mypy ]))
-    rust-analyzer rustc cargo clippy
+    rust-analyzer rustc rustfmt cargo clippy
     nil
     nodePackages.bash-language-server
   ];
