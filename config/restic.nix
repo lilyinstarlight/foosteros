374fcfdc396d0dc20d70b1239160f3343ee9947f
Lily Foster <lily@lily.flowers>
2023-01-06 18:34:22 -0500

    config/restic: use filesystem snapshotting when on btrfs

--- config/restic.nix
+++ config/restic.nix
@@ -1,24 +1,33 @@
 { config, lib, pkgs, ... }:
 
 {
-  # TODO: perform fs snapshot if we can
-  services.restic.backups.${config.networking.hostName} = {
-    initialize = true;
-    repository = "b2:${lib.replaceStrings ["."] ["-"] config.networking.fqdn}-backup";
-    paths = [
-      "/state"
-    ];
-    pruneOpts = [
-      "--keep-daily 7"
-      "--keep-weekly 5"
-      "--keep-monthly 12"
-      "--keep-yearly 2"
-    ];
-    timerConfig = {
-      OnCalendar = "daily";
-      Persistent = true;
-    };
-  };
+  services.restic.backups.${config.networking.hostName} = lib.mkMerge [
+    {
+      initialize = true;
+      repository = "b2:${lib.replaceStrings ["."] ["-"] config.networking.fqdn}-backup";
+      paths = [
+        "/backup"
+      ];
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 12"
+        "--keep-yearly 2"
+      ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
+    }
+    (lib.mkIf (config.fileSystems."/".fsType == "btrfs" && config.fileSystems."/state".fsType == "btrfs") {
+      backupPrepareCommand = ''
+        btrfs subvolume snapshot /state /backup
+      '';
+      backupCleanupCommand = ''
+        btrfs subvolume delete /backup
+      '';
+    })
+  ];
 
   systemd.services."restic-backups-${config.networking.hostName}" = {
     wants = [ "network-online.target" ];
