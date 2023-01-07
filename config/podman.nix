865559acda2935d7535e9da44ac1d9690402903d
Lily Foster <lily@lily.flowers>
2023-01-06 21:30:31 -0500

    config/podman: set default network for rootless too

--- config/podman.nix
+++ config/podman.nix
@@ -6,6 +6,24 @@
     defaultNetwork.settings.dns_enabled = true;
   };
 
+  home-manager.sharedModules = [
+    ({ pkgs, ... }: {
+      xdg.dataFile."containers/storage/networks/podman.json".source = let
+        json = pkgs.formats.json {};
+      in json.generate "podman.json" ({
+          dns_enabled = true;
+          driver = "bridge";
+          id = "0000000000000000000000000000000000000000000000000000000000000000";
+          internal = false;
+          ipam_options = { driver = "host-local"; };
+          ipv6_enabled = false;
+          name = "podman";
+          network_interface = "podman0";
+          subnets = [{ gateway = "10.88.0.1"; subnet = "10.88.0.0/16"; }];
+        });
+    })
+  ];
+
   virtualisation.containers.registries.search = [ "docker.io" ];
 
   environment.systemPackages = with pkgs; [
