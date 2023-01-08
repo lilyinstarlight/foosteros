86c8007eb248055d61194d10060ea7660ae0ab09
Lily Foster <lily@lily.flowers>
2023-01-07 21:55:25 -0500

    config/ledger: fix link ordering

--- config/ledger.nix
+++ config/ledger.nix
@@ -5,10 +5,10 @@
     hledger
   ];
 
-  home-manager.users.lily = { lib, ... }: let cfg = config.home-manager.users.lily; in {
+  home-manager.users.lily = { lib, ... }: {
     home.activation = {
       linkHomeLedger = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        $DRY_RUN_CMD ln -sTf $VERBOSE_ARG "$HOME"/.hledger.journal "$HOME"/docs/ledger/2023.journal
+        $DRY_RUN_CMD ln -sTf $VERBOSE_ARG "$HOME"/docs/ledger/2023.journal "$HOME"/.hledger.journal
       '';
     };
   };
