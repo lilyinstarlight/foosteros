6fdbb2484a487f8d7ceb419a11ec011400055cd7
Lily Foster <lily@lily.flowers>
2023-01-23 12:00:00 -0500

    config/production: temporarily remove gimp plugins

--- config/production.nix
+++ config/production.nix
@@ -2,7 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    inkscape gimp-with-plugins krita
+    # TODO: add back gimp plugins once NixOS/nixpkgs#211600 is merged
+    #inkscape gimp-with-plugins krita
+    inkscape gimp krita
     helvum qjackctl qsynth vmpk calf
     ardour lmms
     sonic-pi sonic-pi-tool open-stage-control
