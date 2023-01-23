91b2753d5e5c08db49254f9a2b97133eb21fa278
Lily Foster <lily@lily.flowers>
2023-01-23 11:59:48 -0500

    comma: do override directly

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -45,9 +45,19 @@ in with outpkgs;
 
   # overridden packages
   # TODO: remove after nix-community/comma#44 is merged
-  comma = import ./comma {
-    inherit (pkgs) comma fetchpatch;
-  };
+  comma = pkgs.comma.overrideAttrs (attrs: {
+    patches = (attrs.patches or []) ++ [
+      # See https://github.com/nix-community/comma/pull/44
+      (fetchpatch {
+        name = "comma-no-unnecessary-existential-warning.patch";
+        url = "https://github.com/nix-community/comma/commit/646e6ed37b64fbc2d3014952645b15d15c9b3aab.patch";
+        hash = "sha256-4kdxjvXKosy9eDLPN05MuWU+SDULBq/AxQAr3gH00Xg=";
+        postFetch = ''
+          sed -i 's/`comma --update`/`--update`/' $out
+        '';
+      })
+    ];
+  });
   monofur-nerdfont = nerdfonts.override {
     fonts = [ "Monofur" ];
   };
