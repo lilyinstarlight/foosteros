bf1616396e828926c00f4e60ad14f705ce34d49f
Lily Foster <lily@lily.flowers>
2023-02-02 12:15:03 -0500

    comma: remove in favor of nixpkgs version

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -50,21 +50,6 @@ in with outpkgs;
   curl-impersonate = callPackage ./curl-impersonate {};
 
   # overridden packages
-  # TODO: remove after nix-community/comma#44 is merged
-  comma = pkgs.comma.overrideAttrs (attrs: {
-    patches = (attrs.patches or []) ++ [
-      # See https://github.com/nix-community/comma/pull/44
-      (fetchpatch {
-        name = "comma-no-unnecessary-existential-warning.patch";
-        url = "https://github.com/nix-community/comma/commit/646e6ed37b64fbc2d3014952645b15d15c9b3aab.patch";
-        hash = "sha256-4kdxjvXKosy9eDLPN05MuWU+SDULBq/AxQAr3gH00Xg=";
-        postFetch = ''
-          sed -i 's/`comma --update`/`--update`/' $out
-        '';
-      })
-    ];
-  });
-
   monofur-nerdfont = nerdfonts.override {
     fonts = [ "Monofur" ];
   };
