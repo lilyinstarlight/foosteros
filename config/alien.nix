5dffb64a59956243aacb26b3849e4fef737488d6
Lily Foster <lily@lily.flowers>
2023-02-06 10:38:40 -0500

    config/alien: remove old todo comment

--- config/alien.nix
+++ config/alien.nix
@@ -15,6 +15,5 @@
   programs.nix-ld.enable = true;
   services.envfs.enable = true;
 
-  # TODO: remove override when checks are fixed
   environment.systemPackages = with inputs.nix-alien.packages.${pkgs.stdenv.hostPlatform.system}; [ nix-alien ];
 }
