54307d31ce227581603d6e167deb083a46b252f5
Lily Foster <lily@lily.flowers>
2023-02-07 13:16:55 -0500

    config/bluetooth: use mkDefault for bluetooth name

--- config/bluetooth.nix
+++ config/bluetooth.nix
@@ -4,7 +4,7 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false;
-    settings.General.Name =
+    settings.General.Name = lib.mkDefault
       (lib.toUpper (lib.substring 0 1 config.networking.hostName)) + (lib.substring 1 (lib.stringLength config.networking.hostName) config.networking.hostName);
   };
 }
