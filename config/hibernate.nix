8baf98e352b6328a1f61a30cbfbb40fd9048ef60
Lily Foster <lily@lily.flowers>
2023-02-17 09:35:19 -0500

    config/hibernate: default to suspend-then-hibernate

--- config/hibernate.nix
+++ config/hibernate.nix
@@ -5,4 +5,10 @@
       eligibleSwaps = (map (swap: if swap ? device then swap.device else "/dev/disk/by-label/${swap.label}")
         (lib.filter (swap: lib.hasPrefix "/dev/" swap.device && !swap.randomEncryption.enable && !(lib.hasPrefix "/dev/zram" swap.device)) config.swapDevices));
     in lib.mkIf ((lib.length eligibleSwaps) > 0) (lib.mkDefault (lib.head eligibleSwaps));
+
+  services.logind.lidSwitch = lib.mkDefault "suspend-then-hibernate";
+
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=30m
+  '';
 }
