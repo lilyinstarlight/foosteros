345468a63e7ffa602ba36616d776bc76025bc44f
Lily Foster <lily@lily.flowers>
2023-03-16 14:31:33 -0400

    config/azure: remove azure-cli for now

--- config/azure.nix
+++ config/azure.nix
@@ -4,6 +4,8 @@
   environment.systemPackages = with pkgs; [
     (ansible.overrideAttrs (attrs: {
       propagatedBuildInputs = attrs.propagatedBuildInputs ++ (with python3Packages; [ passlib ]);
-    })) azure-cli
+    # TODO: remove once azure-cli is fixed in nixos-unstable
+    #})) azure-cli
+    }))
   ];
 }
