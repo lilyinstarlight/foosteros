668447409495a68525af74261d19f39183f71e5a
Lily Foster <lily@lily.flowers>
2023-03-16 20:40:27 -0400

    config/secureboot: remove now-unnecessary fwupd workaround

--- config/secureboot.nix
+++ config/secureboot.nix
@@ -1,10 +1,5 @@
 { config, lib, pkgs, ... }:
 
-# TODO: remove when nix-community/lanzaboote#131 is merged
-let
-  cfg = config.boot.lanzaboote;
-in
-
 {
   environment.systemPackages = with pkgs; [
     sbctl
@@ -15,20 +10,5 @@ in
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/etc/secureboot";
-  };
-
-  # TODO: remove when nix-community/lanzaboote#131 is merged
-  systemd.services.fwupd = lib.mkIf config.services.fwupd.enable {
-    # Tell fwupd to load its efi files from /run
-    environment.FWUPD_EFIAPPDIR = "/run/fwupd-efi";
-    serviceConfig.RuntimeDirectory = "fwupd-efi";
-    # Place the fwupd efi files in /run and sign them
-    preStart = ''
-      cp ${config.services.fwupd.package.fwupd-efi}/libexec/fwupd/efi/fwupd*.efi /run/fwupd-efi/
-      ${pkgs.sbsigntool}/bin/sbsign --key '${cfg.privateKeyFile}' --cert '${cfg.publicKeyFile}' /run/fwupd-efi/fwupd*.efi
-    '';
-  };
-  services.fwupd.uefiCapsuleSettings = lib.mkIf config.services.fwupd.enable {
-    DisableShimForSecureBoot = true;
   };
 }
