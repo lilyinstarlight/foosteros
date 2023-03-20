8627e04be5f5acaa117192cdaf4347a7138d9d91
Lily Foster <lily@lily.flowers>
2023-03-20 09:49:52 -0400

    config/workstation: use ffmpeg-full for workstation mpv

--- config/workstation.nix
+++ config/workstation.nix
@@ -5,7 +5,7 @@
     firefox ungoogled-chromium
     pavucontrol qalculate-gtk
     element-desktop jitsi-meet-electron teams-for-linux webcord
-    ffmpeg-full
+    ffmpeg-full (lib.hiPrio (wrapMpv (mpv-unwrapped.override { ffmpeg_5 = ffmpeg_5-full; }) {}))
     fq ripgrep-all
     mkusb mkwin
     aria2 openssl wireshark dogdns picocom
