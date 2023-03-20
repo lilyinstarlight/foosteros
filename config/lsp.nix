fed828e7b6a523ab3d805482d97afa4c59c9b9eb
Lily Foster <lily@lily.flowers>
2023-03-20 10:42:42 -0400

    config/lsp: make pylsp stuff work without a wrapped python3

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,8 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    (python3.withPackages (ps: with ps; [ python-lsp-server pylsp-mypy ]))
+  environment.systemPackages = with pkgs; let
+    pylsp-env = python3.withPackages (ps: with ps; [ python-lsp-server pylsp-mypy ]);
+    pylsp-with-plugins = runCommand "pylsp" {} ''
+      mkdir -p $out/bin
+      ln -s ${pylsp-env}/bin/pylsp $out/bin/pylsp
+    '';
+  in [
+    pylsp-with-plugins
     rust-analyzer rustc rustfmt cargo clippy
     nil
     nodePackages.bash-language-server
