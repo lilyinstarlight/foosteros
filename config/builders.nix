092f0b0914f6aaecbd68a80d1537b23385c0aef0
Lily Foster <lily@lily.flowers>
2023-03-23 17:42:38 -0400

    config/builders: add builder config

---
+++ config/builders.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix = {
+    settings = {
+      builders-use-substitutes = true;
+    };
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        protocol = "ssh-ng";
+        hostName = "darwin-build-box.winter.cafe";
+        maxJobs = 4;
+        systems = [ "aarch64-darwin" "x86_64-darwin" ];
+        supportedFeatures = [];
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUIwaW85RTBlWGlESUVIdnNpYlhPeE9QdmVTalVQSXIxUm5OS2JVa3czZkQ=";
+        sshUser = "lily";
+        sshKey = "/home/lily/.ssh/id_ed25519";
+      }
+      {
+        protocol = "ssh-ng";
+        hostName = "eu.nixbuild.net";
+        maxJobs = 4;
+        systems = [ "aarch64-linux" ];
+        supportedFeatures = [ "benchmark" "big-parallel" ];
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSVBJUUNaYzU0cG9KOHZxYXdkOFRyYU5yeVFlSm52SDFlTHBJRGdiaXF5bU0=";
+        sshUser = "lily";
+        sshKey = "/home/lily/.ssh/id_ed25519";
+      }
+    ];
+  };
+}
