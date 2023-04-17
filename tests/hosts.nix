4477517acb6b66d2961a45077a82037ab781b4c3
Lily Foster <lily@lily.flowers>
2023-04-17 10:50:16 -0400

    tests: rename checks a little bit

--- tests/hosts.nix
+++ tests/hosts.nix
@@ -17,8 +17,8 @@ let
 in
 
 lib.listToAttrs (lib.flatten (
-  map (cfg: [ { name = "host-test-" + cfg.config.networking.hostName; value = testSystem cfg; } ] ++
-    lib.optionals (cfg.config.system.build ? installer) [ { name = "installer-test-" + cfg.config.networking.hostName; value = testInstaller cfg; } ])
+  map (cfg: [ { name = "host-" + cfg.config.networking.hostName; value = testSystem cfg; } ] ++
+    lib.optionals (cfg.config.system.build ? installer) [ { name = "installer-" + cfg.config.networking.hostName; value = testInstaller cfg; } ])
   (lib.unique (lib.filter (cfg: cfg.pkgs.stdenv.buildPlatform.system == pkgs.stdenv.buildPlatform.system) (lib.collect
     (cfg: cfg ? config && cfg.config ? system && cfg.config.system ? build && cfg.config.system.build ? toplevel)
     self.nixosConfigurations
