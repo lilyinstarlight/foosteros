9968dbf897f0a4a54ef3181e4cccb83517e07a81
Lily Foster <lily@lily.flowers>
2023-07-30 00:30:43 -0400

    config/production: update ndi derivation for 5.6.0

--- config/production.nix
+++ config/production.nix
@@ -10,13 +10,15 @@ lib.mkIf config.foosteros.profiles.production {
     (wrapOBS {
       plugins = with obs-studio-plugins; [ wlrobs obs-gstreamer obs-move-transition ] ++ (lib.optionals pkgs.config.allowUnfree [ (obs-ndi.override {
         ndi = ndi.overrideAttrs (attrs: rec {
+          version = "5.6.0";
+
           src = fetchurl {
-            name = "${attrs.pname}-${attrs.version}.tar.gz";
+            name = "${attrs.pname}-${version}.tar.gz";
             url = "https://downloads.ndi.tv/SDK/NDI_SDK_Linux/Install_NDI_SDK_v5_Linux.tar.gz";
             hash = "sha256-flxUaT1q7mtvHW1J9I1O/9coGr0hbZ/2Ab4tVa8S9/U=";
           };
 
-          unpackPhase = ''unpackFile ${src}; echo y | ./${attrs.installerName}.sh; sourceRoot="NDI SDK for Linux";'';
+          installPhase = lib.concatStringsSep "\n" (lib.filter (line: !(lib.hasPrefix "mv logos " line)) (lib.splitString "\n" attrs.installPhase));
         });
       }) ]);
     })
