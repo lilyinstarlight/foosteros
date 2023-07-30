9c9f001e5b2633a163871912d25590e1938cee0a
Lily Foster <lily@lily.flowers>
2023-07-30 00:35:29 -0400

    hosts/bina: temporarily disable systemd-254 PR changes

--- hosts/bina/hardware.nix
+++ hosts/bina/hardware.nix
@@ -6,8 +6,8 @@
   ];
 
   # TODO: remove when NixOS/nixpkgs#243242 is merged
-  systemd.package = inputs.systemd-254.legacyPackages.${pkgs.stdenv.hostPlatform.system}.systemd;
-  boot.initrd.systemd.package = inputs.systemd-254.legacyPackages.${pkgs.stdenv.hostPlatform.system}.systemd;
+  #systemd.package = inputs.systemd-254.legacyPackages.${pkgs.stdenv.hostPlatform.system}.systemd;
+  #boot.initrd.systemd.package = inputs.systemd-254.legacyPackages.${pkgs.stdenv.hostPlatform.system}.systemd;
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
 
