4d447d42338a0b2509bae8de1749b93e7fa96340
Lily Foster <lily@lily.flowers>
2023-07-30 08:18:27 -0400

    hosts/bina: re-add systemd 254 for testing

--- hosts/bina/hardware.nix
+++ hosts/bina/hardware.nix
@@ -6,8 +6,9 @@
   ];
 
   # TODO: remove when NixOS/nixpkgs#243242 is merged
-  #systemd.package = inputs.systemd-254.legacyPackages.${pkgs.stdenv.hostPlatform.system}.systemd;
-  #boot.initrd.systemd.package = inputs.systemd-254.legacyPackages.${pkgs.stdenv.hostPlatform.system}.systemd;
+  systemd.package = inputs.systemd-254.legacyPackages.${pkgs.stdenv.hostPlatform.system}.systemd;
+  boot.initrd.systemd.package = inputs.systemd-254.legacyPackages.${pkgs.stdenv.hostPlatform.system}.systemd;
+  boot.initrd.systemd.suppressedUnits = [ "systemd-hibernate-resume@.service" ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
 
