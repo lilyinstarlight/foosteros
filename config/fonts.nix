466b3f491aa51110f4326b8c43d07b4003e86f22
Lily Foster <lily@lily.flowers>
2023-09-10 10:01:52 -0400

    config/fonts: add emoji first

--- config/fonts.nix
+++ config/fonts.nix
@@ -2,8 +2,9 @@
 
 lib.mkIf config.foosteros.profiles.fonts {
   fonts.packages = with pkgs; [
+    noto-fonts-emoji
     aileron
-    noto-fonts noto-fonts-cjk-sans noto-fonts-emoji
+    noto-fonts noto-fonts-cjk-sans
     dejavu_fonts
     freefont_ttf
     gyre-fonts
