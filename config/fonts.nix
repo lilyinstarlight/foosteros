7f8d18db36ffa3fc08cf0de5f160807424432805
Lily Foster <lily@lily.flowers>
2023-09-10 20:09:22 -0400

    config/fonts: make emoji font higher precedent for monospace font

--- config/fonts.nix
+++ config/fonts.nix
@@ -2,13 +2,17 @@
 
 lib.mkIf config.foosteros.profiles.fonts {
   fonts.packages = with pkgs; [
-    noto-fonts-emoji
     aileron
-    noto-fonts noto-fonts-cjk-sans
+    noto-fonts noto-fonts-cjk-sans noto-fonts-emoji
     dejavu_fonts
     freefont_ttf
     gyre-fonts
     liberation_ttf
     roboto comfortaa
+  ];
+
+  fonts.fontconfig.defaultFonts.monospace = lib.mkDefault [
+    "Noto Color Emoji"
+    "DejaVu Sans Mono"
   ];
 }
