b940d56faaae1fa206b2f34ddc874f67744c7e50
Lily Foster <lily@lily.flowers>
2023-11-12 20:53:10 -0500

    flake,tests: ignore stdenvs

--- tests/pkgs.nix
+++ tests/pkgs.nix
@@ -5,7 +5,7 @@ with pkgs;
 lib.listToAttrs (lib.flatten (
   map (drv: [ { name = "pkg-" + (if drv ? pname then drv.pname else drv.name); value = drv; } ] ++
     lib.optionals (drv ? tests) (lib.mapAttrsToList (name: value: { name = "pkg-" + (if drv ? pname then drv.pname else drv.name) + "-test-" + name; inherit value; }) drv.tests))
-  (lib.unique (lib.filter (drv: !drv.meta.unsupported && !drv.meta.broken && !drv.meta.unfree && (drv.meta ? dependsUnfree -> !drv.meta.dependsUnfree)) (lib.collect
+  (lib.unique (lib.filter (drv: drv.meta ? position && !drv.meta.unsupported && !drv.meta.broken && !drv.meta.unfree && (drv.meta ? dependsUnfree -> !drv.meta.dependsUnfree)) (lib.collect
     (drv: lib.isDerivation drv)
     self.legacyPackages.${pkgs.stdenv.buildPlatform.system}
   )))
