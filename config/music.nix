0b91b1aec2c527b0ae5b5c226548c43748c4d841
Lily Foster <lily@lily.flowers>
2023-11-17 21:13:41 -0500

    config/music: remove mopidy-spotify override

--- config/music.nix
+++ config/music.nix
@@ -15,17 +15,7 @@ lib.mkIf config.foosteros.profiles.music {
         local.media_dir = "${config.home.homeDirectory}/music";
       };
       extensionPackages = with pkgs; [
-        mopidy-local mopidy-iris mopidy-mpris mopidy-notify mopidy-mpd #mopidy-spotify
-        #TODO: remove and restore above when NixOS/nixpkgs#265688 is merged
-        (mopidy-spotify.overrideAttrs (attrs: {
-          version = "unstable-2023-11-01";
-          src = fetchFromGitHub {
-            owner = "mopidy";
-            repo = "mopidy-spotify";
-            rev = "48faaaa2642647b0152231798b46ccd9631694f5";
-            hash = "sha256-RwkUdcbDU7/ndVnPteG/iXB2dloljvCHQlvPk4tacuA=";
-          };
-        }))
+        mopidy-local mopidy-iris mopidy-mpris mopidy-notify mopidy-mpd mopidy-spotify
       ];
     };
 
