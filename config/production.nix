1ef7c0d6cf3cfdc87d89e41c9173704512628561
Lily Foster <lily@lily.flowers>
2023-11-21 21:48:53 -0500

    config/production: just remove ndi given it is no longer being used

--- config/production.nix
+++ config/production.nix
@@ -8,20 +8,7 @@ lib.mkIf config.foosteros.profiles.production {
     sonic-pi sonic-pi-tool open-stage-control
     lilypond
     (wrapOBS {
-      plugins = with obs-studio-plugins; [ wlrobs obs-gstreamer obs-move-transition obs-backgroundremoval ] ++ (lib.optionals pkgs.config.allowUnfree [ (obs-ndi.override {
-        # TODO: remove override when NixOS/nixpkgs#247094 is merged
-        ndi = ndi.overrideAttrs (attrs: rec {
-          version = "5.6.0";
-
-          src = fetchurl {
-            name = "${attrs.pname}-${version}.tar.gz";
-            url = "https://downloads.ndi.tv/SDK/NDI_SDK_Linux/Install_NDI_SDK_v5_Linux.tar.gz";
-            hash = "sha256-T/S5LyxfQtI0qn0ULi3n6bBFxytGrVFJpFnUjv2SGN4=";
-          };
-
-          installPhase = lib.concatStringsSep "\n" (lib.filter (line: !(lib.hasPrefix "mv logos " line)) (lib.splitString "\n" attrs.installPhase));
-        });
-      }) ]);
+      plugins = with obs-studio-plugins; [ wlrobs obs-gstreamer obs-move-transition obs-backgroundremoval ];
     })
   ];
 }
