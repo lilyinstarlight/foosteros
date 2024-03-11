dcd030c44f4390d021c4260fcdc08cd0446c2f7c
Lily Foster <lily@lily.flowers>
2024-03-11 16:30:58 -0400

    config/production: update software plugins

--- config/production.nix
+++ config/production.nix
@@ -2,14 +2,13 @@
 
 lib.mkIf config.foosteros.profiles.production {
   environment.systemPackages = with pkgs; [
-    inkscape gimp-with-plugins krita
+    inkscape (gimp-with-plugins.override { plugins = with gimpPlugins; [ lqrPlugin gmic ]; }) krita
     helvum qjackctl qsynth vmpk calf
     ardour lmms
     sonic-pi sonic-pi-tool open-stage-control
     lilypond
     (wrapOBS {
-      # TODO: re-add obs-backgroundremoval once NixOS/nixpkgs#258392 is in locked nixpkgs flake
-      plugins = with obs-studio-plugins; [ wlrobs obs-gstreamer obs-move-transition /*obs-backgroundremoval*/ ];
+      plugins = with obs-studio-plugins; [ wlrobs obs-gstreamer obs-move-transition obs-backgroundremoval ];
     })
   ];
 }
