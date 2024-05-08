6dedb3104504e12d52dca001fe4fc2578ab3b8cd
Lily Foster <lily@lily.flowers>
2024-05-08 10:08:55 -0400

    hosts/bina: add cosmic substituter for testing

--- hosts/bina/configuration.nix
+++ hosts/bina/configuration.nix
@@ -177,6 +177,8 @@
 
   nix = {
     settings = {
+      substituters = [ "https://cosmic.cachix.org/" ];
+      trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
       keep-outputs = true;
       max-jobs = "auto";
     };
