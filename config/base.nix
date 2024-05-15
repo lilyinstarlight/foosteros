43dc8c3dddbeac28de7926564efacf28e0ce2d7f
Lily Foster <lily@lily.flowers>
2024-05-15 13:24:36 -0400

    config/base: add git-gr

--- config/base.nix
+++ config/base.nix
@@ -179,7 +179,7 @@ lib.mkIf config.foosteros.profiles.base {
     man-pages man-pages-posix
     dbus file python3 rsync strace tree unzip xxd
     cachix fpaste ftmp furi
-    git git-absorb delta fd ripgrep nix-output-monitor
+    git git-absorb git-gr delta fd ripgrep nix-output-monitor
     shellcheck progress libqalculate
   ];
 
