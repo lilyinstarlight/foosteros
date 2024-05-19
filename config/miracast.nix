49abc4b28340a81e88a2f82d82eb198292f888ea
Lily Foster <lily@lily.flowers>
2024-05-19 12:56:19 -0400

    config/miracast: revert gnome-network-displays version for now

--- config/miracast.nix
+++ config/miracast.nix
@@ -4,6 +4,18 @@ lib.mkIf config.foosteros.profiles.miracast {
   networking.firewall.trustedInterfaces = [ "p2p-wl+" ];
 
   environment.systemPackages = with pkgs; [
-    gnome-network-displays
+    # TODO: revert once sway portal has support for RemoteDesktop
+    #gnome-network-displays
+    (gnome-network-displays.overrideAttrs (old: {
+      version = "0.91.0-unstable-2024-01-27";
+      src = fetchFromGitLab {
+        domain = "gitlab.gnome.org";
+        owner = "GNOME";
+        repo = "gnome-network-displays";
+        rev = "512e9eb6e57fa2c45db983c3d70033514630578c";
+        hash = "sha256-AEq+eS0GF2mBBvldnJs/+f4pHCOPfiOeY6xDO0yeoQU=";
+      };
+      env.NIX_CFLAGS_COMPILE = old.env.NIX_CFLAGS_COMPILE or "" + " -Wno-format-security";
+    }))
   ];
 }
