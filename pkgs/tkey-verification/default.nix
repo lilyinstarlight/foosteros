2cf2e2134ed481899d3a9fe38c91cb528ede029c
Lily Foster <lily@lily.flowers>
2024-07-23 16:09:44 -0400

    tkey-verification: 0.0.3 -> 1.0.0

--- pkgs/tkey-verification/default.nix
+++ pkgs/tkey-verification/default.nix
@@ -1,43 +1,25 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
-, fetchurl
+, nix-update-script
 }:
 
 buildGoModule rec {
   pname = "tkey-verification";
-  version = "0.0.3";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "tillitis";
     repo = "tkey-verification";
     rev = "v${version}";
-    hash = "sha256-35+r+OxBLb+wF2S7LM4towlxmToe+UtzXP340pR58b0=";
+    hash = "sha256-WoV0AsMWMupRW+rJpsD28zGdASzeqQmIu9OGvFNcSW4=";
   };
 
-  vendorHash = "sha256-fiFG16njTHbdSXnNtMSjqKdlTtqL3YZRRaPKuKt02xg=";
+  vendorHash = "sha256-ikCn68wh+46KCEAHjlt7ATrIcPyIpL/WwR0b0rfdWfY=";
 
   subPackages = [ "cmd/tkey-verification" ];
 
-  env = rec {
-    # Requires same hash as upstream blob and building ourselves
-    # is prohibitively difficult to reproduce bit-for-bit an
-    # upstream oci container build.
-    VERISIGNER_VERSION = "0.0.3";
-    VERISIGNER_BIN = fetchurl {
-      # TODO: restore when newer versions have verisigner release asset or tillitis/tkey-verification#19 is merged
-      #url = "https://github.com/tillitis/tkey-verification/releases/download/v${version}/verisigner-v${VERISIGNER_VERSION}.bin";
-      url = "https://github.com/tillitis/tkey-verification/releases/download/v0.0.2/verisigner-v${VERISIGNER_VERSION}.bin";
-      hash = "sha256-g6VNQ1nGjhVjUeCgjZc4U8+BRVAe6F4NfAKB25s4dv0=";
-    };
-  };
-
-  preConfigure = ''
-    mv ./vendor-signing-pubkeys.txt ./internal/vendorsigning/vendor-signing-pubkeys.txt
-    cp -r "$VERISIGNER_BIN" "internal/appbins/bins/verisigner-v''${VERISIGNER_VERSION}".bin
-  '';
-
-  passthru.updateScript = ./update.sh;
+  passthru.updateScript = nix-update-script {};
 
   meta = with lib; {
     description = "A tool used for signing a TKey identity and verifying that it still has the same identity as it did when it was produced by Tillitis";
