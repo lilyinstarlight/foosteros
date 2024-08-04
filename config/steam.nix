73c957115c2cd86f4209f22e9ecf734267ed5a27
Lily Foster <lily@lily.flowers>
2024-08-04 14:11:05 -0400

    config/steam: fix eval when unfree

--- config/steam.nix
+++ config/steam.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 lib.mkIf config.foosteros.profiles.steam {
-  programs.steam = {
+  programs.steam = lib.mkIf pkgs.config.allowUnfree {
     enable = true;
     extest.enable = true;
   };
