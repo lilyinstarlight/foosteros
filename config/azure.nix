652a9377ff9313ff1c55bdb3e84ec14809db38b0
Lily Foster <lily@lily.flowers>
2024-10-16 17:58:03 -0400

    config/azure: temporarily remove azure-cli

--- config/azure.nix
+++ config/azure.nix
@@ -4,6 +4,7 @@ lib.mkIf config.foosteros.profiles.azure {
   environment.systemPackages = with pkgs; [
     (ansible.overrideAttrs (attrs: {
       propagatedBuildInputs = attrs.propagatedBuildInputs ++ (with python3Packages; [ passlib ]);
-    })) azure-cli
+    })) #azure-cli
+    # TODO: uncomment above when https://github.com/NixOS/nixpkgs/pull/345326 reaches unstable
   ];
 }
