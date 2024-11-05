c0f4fd43685ade9cf3922d71205083c5ddc4c4bd
Lily Foster <lily@lily.flowers>
2024-11-05 09:50:55 -0500

    config/azure: re-add azure-cli

--- config/azure.nix
+++ config/azure.nix
@@ -4,7 +4,6 @@ lib.mkIf config.foosteros.profiles.azure {
   environment.systemPackages = with pkgs; [
     (ansible.overrideAttrs (attrs: {
       propagatedBuildInputs = attrs.propagatedBuildInputs ++ (with python3Packages; [ passlib ]);
-    })) #azure-cli
-    # TODO: uncomment above when https://github.com/NixOS/nixpkgs/pull/345326 reaches unstable
+    })) azure-cli
   ];
 }
