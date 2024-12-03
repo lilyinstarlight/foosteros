0a9baef84a4cdf190a24711e7f0f1992e9e1bd02
Lily Foster <lily@lily.flowers>
2024-12-02 19:51:45 -0500

    flake: update inputs

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "";
     };
 
     preservation.url = "github:WilliButz/preservation";
