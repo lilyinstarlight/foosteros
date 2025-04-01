9ef341c49a6bc9f9e5ccfbe1d37e270fa1b3dea2
Lily Foster <lily@lily.flowers>
2025-04-01 14:06:52 -0400

    config/base: remove repl-flake experimental-feature

--- config/base.nix
+++ config/base.nix
@@ -106,7 +106,7 @@ lib.mkIf config.foosteros.profiles.base {
       auto-allocate-uids = true;
       substituters = [ "https://foosteros.cachix.org" ];
       trusted-public-keys = [ "foosteros.cachix.org-1:rrDalTfOT1YohJXiMv8upgN+mFLKZp7eWW1+OGbPRww=" ];
-      experimental-features = [ "nix-command" "flakes" "repl-flake" "auto-allocate-uids" ];
+      experimental-features = [ "nix-command" "flakes" "auto-allocate-uids" ];
       flake-registry = "${inputs.flake-registry}/flake-registry.json";
     };
 
