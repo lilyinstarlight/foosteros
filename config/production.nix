969654ec8cd00385ca1b43f6c5c5141d9008768c
Lily Foster <lily@lily.flowers>
2025-08-27 11:27:57 -0400

    config/production: temporarily remove lmms until it is fixed

--- config/production.nix
+++ config/production.nix
@@ -4,7 +4,9 @@ lib.mkIf config.foosteros.profiles.production {
   environment.systemPackages = with pkgs; [
     inkscape (gimp-with-plugins.override { plugins = with gimpPlugins; [ lqrPlugin gmic ]; }) krita
     helvum qjackctl qsynth vmpk calf
-    ardour lmms
+    # TODO: re-add lmms once NixOS/nixpkgs#437077 is fixed
+    #ardour lmms
+    ardour
     # TODO: re-add open-stage-control once NixOS/nixpkgs#408849 is fixed and sonic-pi-tool once it works with newer sonic-pi
     #sonic-pi sonic-pi-tool open-stage-control
     sonic-pi
