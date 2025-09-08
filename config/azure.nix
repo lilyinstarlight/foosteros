89f8bc5e378519f789e76cd5f5a598cfe0f6111f
Lily Foster <lily@lily.flowers>
2025-09-08 16:25:33 -0400

    config/azure: re-add azure-cli and ansible

--- config/azure.nix
+++ config/azure.nix
@@ -2,9 +2,8 @@
 
 lib.mkIf config.foosteros.profiles.azure {
   environment.systemPackages = with pkgs; [
-    # TODO: re-add azure-cli when NixOS/nixpkgs#437525 is fixed
-    #(ansible.overrideAttrs (attrs: {
-    #  propagatedBuildInputs = attrs.propagatedBuildInputs ++ (with python3Packages; [ passlib ]);
-    #})) azure-cli
+    (ansible.overrideAttrs (attrs: {
+      propagatedBuildInputs = attrs.propagatedBuildInputs ++ (with python3Packages; [ passlib ]);
+    })) azure-cli
   ];
 }
