74f74ce4bb3d50cf9e670fa3120be892ec5c6390
Lily Foster <lily@lily.flowers>
2025-09-08 16:27:10 -0400

    config/azure: add ansible-lint

--- config/azure.nix
+++ config/azure.nix
@@ -4,6 +4,6 @@ lib.mkIf config.foosteros.profiles.azure {
   environment.systemPackages = with pkgs; [
     (ansible.overrideAttrs (attrs: {
       propagatedBuildInputs = attrs.propagatedBuildInputs ++ (with python3Packages; [ passlib ]);
-    })) azure-cli
+    })) ansible-lint azure-cli
   ];
 }
