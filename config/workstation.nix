e6dc4c72a98ff4bfaca35efd2fb7de4cb5d02647
Lily Foster <lily@lily.flowers>
2026-01-10 20:21:45 -0500

    config/workstation: temporarily pin nodejs version for webcord

--- config/workstation.nix
+++ config/workstation.nix
@@ -4,7 +4,9 @@ lib.mkIf config.foosteros.profiles.workstation {
   environment.systemPackages = with pkgs; [
     firefox ungoogled-chromium
     pavucontrol qalculate-gtk
-    (element-desktop.override { element-web = element-web.override { conf.show_labs_settings = true; }; }) mattermost-desktop teams-for-linux webcord
+    # TODO: remove when NixOS/nixpkgs#476071 is fixed
+    #(element-desktop.override { element-web = element-web.override { conf.show_labs_settings = true; }; }) mattermost-desktop teams-for-linux webcord
+    (element-desktop.override { element-web = element-web.override { conf.show_labs_settings = true; }; }) mattermost-desktop teams-for-linux (webcord.override { buildNpmPackage = buildNpmPackage.override { nodejs = nodejs_22; }; })
     ffmpeg-full (lib.hiPrio (mpv.override { mpv-unwrapped = mpv-unwrapped.override { ffmpeg = ffmpeg-full; }; }))
     fq ripgrep-all
     mkusb mkwin
