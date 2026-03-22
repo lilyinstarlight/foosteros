80915262b9692d1b956cd3b1289c9042cc0607eb
Lily Foster <lily@lily.flowers>
2026-03-22 10:19:12 -0400

    tkey-devtools: bump to 0.0.2

--- pkgs/tkey-devtools/default.nix
+++ pkgs/tkey-devtools/default.nix
@@ -1,34 +1,66 @@
 { lib
+, stdenv
 , buildGoModule
 , fetchFromGitHub
+, udev
 , makeBinaryWrapper
 , podman
 , socat
 , nix-update-script
 }:
 
-buildGoModule rec {
+stdenv.mkDerivation (finalAttrs: let
+  mkCmd = name: args: buildGoModule (args // {
+    pname = "${finalAttrs.pname}-${name}";
+
+    inherit (finalAttrs) version src;
+
+    modRoot = "cmd/${name}";
+
+    ldflags = (args.ldflags or []) ++ [
+      "-X main.version=${finalAttrs.version}"
+    ];
+
+    meta = finalAttrs.meta // {
+      mainProgram = name;
+    };
+  });
+in rec {
   pname = "tkey-devtools";
-  version = "0.0.1";
+  version = "0.0.2";
 
   src = fetchFromGitHub {
     owner = "tillitis";
     repo = "tkey-devtools";
     rev = "v${version}";
-    hash = "sha256-ClWL4aNu9qcwENK4NmlsnjAYAKQE+Hq57gN++nOJTRE=";
+    hash = "sha256-hKdH+UQsonXG6Iet3vYaKWzSWOs4o1j1zMcmmG964yA=";
   };
 
-  vendorHash = "sha256-c53xOeQTpo/J2k+kEYei0q3QaD4rqB+9EZR+s0yNLik=";
+  passthru.cmdList = {
+    tkey-runapp = {
+      vendorHash = "sha256-kJkUe2wxRtRgH2Ib4v6xYGTZC8KDINOgfaf3Uvw5+1s=";
+    };
+    hidread = {
+      buildInputs = [ udev ];
+
+      vendorHash = "sha256-DxCxv56D0bGQBZEvmRtoUP+C0rPRN6DLun1nyYfOFhU=";
+    };
+  };
 
   nativeBuildInputs = [ makeBinaryWrapper ];
 
-  subPackages = [ "cmd/tkey-runapp" ];
+  dontConfigure = true;
+  dontBuild = true;
 
-  ldflags = [
-    "-X main.version=${version}"
-  ];
+  installPhase = ''
+    runHook preInstall
 
-  postInstall = ''
+    mkdir -p $out/bin
+
+    for cmd in ${lib.escapeShellArgs (lib.mapAttrsToList (name: args: lib.getExe (mkCmd name args)) finalAttrs.passthru.cmdList)}; do
+      ln -s "$cmd" $out/bin/
+    done
+
     install -Dm755 run-tkey-qemu $out/bin/run-tkey-qemu
 
     mkdir -p $out/lib/udev/rules.d
@@ -36,9 +68,12 @@ buildGoModule rec {
 
     wrapProgram $out/bin/run-tkey-qemu \
       --prefix PATH : ${lib.makeBinPath [ podman socat ]}
+
+    runHook postInstall
   '';
 
-  passthru.updateScript = nix-update-script {};
+  # TODO: test if this still works?
+  #passthru.updateScript = nix-update-script {};
 
   meta = with lib; {
     description = "Development tools for the Tillitis TKey USB security token";
@@ -48,4 +83,4 @@ buildGoModule rec {
     maintainers = with maintainers; [ /*lilyinstarlight*/ ];
     mainProgram = "tkey-runapp";
   };
-}
+})
