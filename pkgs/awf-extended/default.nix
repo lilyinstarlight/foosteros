2ce7c86ce4abaf98ca7764ec717803be3bb1833b
Lily Foster <lily@lily.flowers>
2026-04-18 12:37:03 +0200

    awf-extended: add qt variants

--- pkgs/awf-extended/default.nix
+++ pkgs/awf-extended/default.nix
@@ -8,36 +8,85 @@
 , gtk3
 , gtk4
 , libnotify
+, qt6
+, qt5
 , gitUpdater
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "awf-extended";
   version = "4.0.0";
 
   src = fetchFromGitHub {
     owner = "luigifab";
     repo = "awf-extended";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-8v+cV1YKlNLz0vrKrflz6eF5UKIHD8QZhutKd4bn6Dg=";
   };
 
-  nativeBuildInputs = [ autoreconfHook pkg-config wrapGAppsHook3 ];
+  dontConfigure = true;
+  dontBuild = true;
 
-  buildInputs = [ gtk2 gtk3 gtk4 libnotify ];
+  installPhase = ''
+    runHook preInstall
 
-  postPatch = ''
-    substituteInPlace src/Makefile.am \
-      --replace '/usr/include/gtk-4.0/unix-print' '${lib.getDev gtk4}/include/gtk-4.0/unix-print' \
-      --replace '/usr/include/gtk-3.0/unix-print' '${lib.getDev gtk3}/include/gtk-3.0/unix-print' \
-      --replace '/usr/include/gtk-unix-print-2.0' '${lib.getDev gtk2}/include/gtk-unix-print-2.0'
+    mkdir -p $out/bin
+    cp -t $out/bin \
+      ${finalAttrs.passthru.gtk}/bin/* \
+      ${finalAttrs.passthru.qt6}/bin/* \
+      ${finalAttrs.passthru.qt5}/bin/*
 
-    touch NEWS AUTHORS README ChangeLog
-    mv LICENSE COPYING
+    runHook postInstall
   '';
 
-  passthru.updateScript = gitUpdater {
-    rev-prefix = "v";
+  passthru = {
+    gtk = stdenv.mkDerivation {
+      pname = finalAttrs.pname + "-gtk";
+      inherit (finalAttrs) version;
+
+      inherit (finalAttrs) src;
+
+      nativeBuildInputs = [ autoreconfHook pkg-config wrapGAppsHook3 ];
+      buildInputs = [ gtk2 gtk3 gtk4 libnotify ];
+
+      postPatch = ''
+        substituteInPlace src/Makefile.am \
+          --replace '/usr/include/gtk-4.0/unix-print' '${lib.getDev gtk4}/include/gtk-4.0/unix-print' \
+          --replace '/usr/include/gtk-3.0/unix-print' '${lib.getDev gtk3}/include/gtk-3.0/unix-print' \
+          --replace '/usr/include/gtk-unix-print-2.0' '${lib.getDev gtk2}/include/gtk-unix-print-2.0'
+
+        touch NEWS AUTHORS README ChangeLog
+        mv LICENSE COPYING
+      '';
+
+      inherit (finalAttrs) meta;
+    };
+
+    qt6 = finalAttrs.passthru.gtk.overrideAttrs {
+      pname = finalAttrs.pname + "-qt6";
+
+      nativeBuildInputs = [ autoreconfHook pkg-config qt6.wrapQtAppsHook ];
+      buildInputs = [ qt6.qtbase ];
+
+      configureFlags = [
+        "--enable-only-qt6"
+      ];
+    };
+
+    qt5 = finalAttrs.passthru.gtk.overrideAttrs {
+      pname = finalAttrs.pname + "-qt5";
+
+      nativeBuildInputs = [ autoreconfHook pkg-config qt5.wrapQtAppsHook ];
+      buildInputs = [ qt5.qtbase ];
+
+      configureFlags = [
+        "--enable-only-qt5"
+      ];
+    };
+
+    updateScript = gitUpdater {
+      rev-prefix = "v";
+    };
   };
 
   meta = with lib; {
@@ -54,4 +103,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ /*lilyinstarlight*/ ];
     mainProgram = "awf-gtk4";
   };
-}
+})
