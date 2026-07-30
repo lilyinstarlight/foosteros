061b5aa9738fac5d76a52d501de3c3c3398aa483
Lily Foster <lily@lily.flowers>
2026-07-30 19:14:21 -0400

    config/lsp: temp fix pylsp for now

--- config/lsp.nix
+++ config/lsp.nix
@@ -2,7 +2,9 @@
 
 lib.mkIf config.foosteros.profiles.lsp {
   environment.systemPackages = with pkgs; let
-    pylsp-env = python3.withPackages (ps: with ps; [ python-lsp-server pylsp-mypy ]);
+    # TODO: re-enable pylsp-mypy when NixOS/nixpkgs#541116 is merged or build otherwise fixed
+    #pylsp-env = python3.withPackages (ps: with ps; [ python-lsp-server pylsp-mypy ]);
+    pylsp-env = python3.withPackages (ps: with ps; [ python-lsp-server ]);
     pylsp-with-plugins = runCommand "pylsp" {} ''
       mkdir -p $out/bin
       ln -s ${pylsp-env}/bin/pylsp $out/bin/pylsp
