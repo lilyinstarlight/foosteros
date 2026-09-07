ed04e7a651b10f12331a85db40283a55073975d5
Lily Foster <lily@lily.flowers>
2026-09-07 13:54:21 -0400

    pkgs: init rofi-pass

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -48,6 +48,8 @@ in with outpkgs;
     syslinux = if stdenv.hostPlatform.isx86_64 then syslinux else pkgsCross.gnu64.syslinux;
   };
   mkwin = callPackage ./mkwin {};
+  rofi-pass = callPackage ./rofi-pass {};
+  rofi-pass-wayland = rofi-pass.override { backend = "wayland"; };
   swaylock-fprintd = callPackage ./swaylock-fprintd {};
   tkey-libs = callPackage ./tkey-libs {};
   tkey-devtools = callPackage ./tkey-devtools {};
