46017cfafdfcf456d7d5a506b99b41c05c25042c
Lily Foster <lily@lily.flowers>
2026-09-16 20:11:55 -0400

    config/preservation: ensure machine-id exists to be initialized on first boot

--- config/preservation.nix
+++ config/preservation.nix
@@ -23,6 +23,37 @@ lib.mkIf config.foosteros.profiles.preservation {
     };
   };
 
+  boot.initrd.systemd.services.ensure-machine-id = {
+    description = "Ensure machine-id Existence";
+
+    requires = [ "initrd-root-device.target" ];
+    after = [ "local-fs-pre.target" "initrd-root-device.target" ];
+    requiredBy = [ "initrd-root-fs.target" ];
+    before = [ "sysroot.mount" ];
+
+    unitConfig = {
+      AssertPathExists = "/etc/initrd-release";
+      DefaultDependencies = false;
+    };
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    script = ''
+      mkdir -p /run/statevol
+      mount -t btrfs -o rw,subvol=/state ${lib.escapeShellArg (if config.fileSystems."/state" ? device then config.fileSystems."/state".device else "/dev/disk/by-label/${config.fileSystems."/state".label}")} /run/statevol
+
+      if ! [ -f /run/statevol/etc/machine-id ]; then
+        printf 'uninitialized\n' >/run/statevol/etc/machine-id
+      fi
+
+      umount /run/statevol
+      rmdir /run/statevol
+    '';
+  };
+
   systemd.services.systemd-machine-id-commit = {
     unitConfig.ConditionPathIsMountPoint = [
       "" "/state/etc/machine-id"
